c9e9899c981624c832d085ba4b514469a73c48ba
teguh triatmojo <49945200+T36UHTR14TM0J0@users.noreply.github.com>
2025-04-28 00:04:34 +0700

    update template

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -63,7 +63,7 @@
     <!-- Tombol Submit -->
     <div class="form-group row mb-0">
         <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-indigo">
+            <button type="submit" class="btn btn-sm btn-primary">
                 Simpan Perubahan
             </button>
         </div>
