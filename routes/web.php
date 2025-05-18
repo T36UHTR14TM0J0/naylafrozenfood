3ea87e296ddbf74f6dff73f1d2df29193b37ee8b
teguh triatmojo <49945200+T36UHTR14TM0J0@users.noreply.github.com>
2025-05-19 01:42:41 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use App\Http\Controllers\SatuanController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserController;
 
-// Auth Routes
+
+// use App\Http\Controllers\Admin\AdminController;
+// use App\Http\Controllers\Owner\OwnerController;
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
@@ -21,8 +24,6 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Register Routes
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
@@ -32,46 +33,31 @@ Route::post('forgot_password', [AuthController::class, 'sendResetLinkEmail'])->n
 Route::get('reset_password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('password.reset');
 Route::post('reset_password', [AuthController::class, 'resetPassword'])->name('password.update');
 
-// Routes yang memerlukan autentikasi
 Route::middleware(['auth'])->group(function () {
-    // Profile Routes
+    // Route untuk ProfileController yang kita buat sebelumnya
     Route::prefix('profile')->group(function () {
         Route::get('edit', [ProfileController::class, 'index'])->name('profile.edit');
         Route::put('update', [ProfileController::class, 'update'])->name('profile.update');
     });
 
-    // Dashboard Routes
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
-
 // Admin Routes
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
-    // Kategori Routes
     Route::resource('kategori', KategoriController::class);
-    
-    // Satuan Routes
     Route::resource('satuan', SatuanController::class);
-
-    // Item Routes
     Route::resource('item', ItemController::class);
-    
-    // Item Stock Routes
     Route::resource('stok', ItemStockController::class);
-    
-    // Transaksi Routes
     Route::get('/transaksi', [TransaksiController::class, 'index'])->name('transaksi.index');
     Route::post('/transaksi/store', [TransaksiController::class, 'store'])->name('transaksi.store');
-    
-    // QRIS Transaction Routes
+
     Route::post('/create-qris-transaction', [TransaksiController::class, 'createTransaction'])->name('create.qris.transaction');
-    
-    // Callback Routes
-    Route::match(['GET', 'POST'], '/transaksi/callback', [TransaksiController::class, 'handleCallback'])->name('transaksi.callback');
+    Route::post('/transaksi/callback', [TransaksiController::class, 'handleCallback'])
+     ->name('transaksi.callback');
 });
 
-// Owner Routes
+// // Owner Routes
 Route::prefix('owner')->middleware(['auth', 'owner'])->group(function () {
-    // User Management Routes
     Route::get('user', [UserController::class, 'index'])->name('user.index');
     Route::get('user/create', [UserController::class, 'create'])->name('user.create');
     Route::post('user', [UserController::class, 'store'])->name('user.store');
@@ -80,6 +66,5 @@ Route::prefix('owner')->middleware(['auth', 'owner'])->group(function () {
     Route::put('/user/{user}', [UserController::class, 'update'])->name('user.update');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 
-    // Supplier Routes
     Route::resource('supplier', SupplierController::class);
 });
