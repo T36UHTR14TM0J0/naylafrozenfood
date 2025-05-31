bd8b3eb9a6d3378b246ce396380aa83136fea6f4
teguh triatmojo <49945200+T36UHTR14TM0J0@users.noreply.github.com>
2025-05-31 19:27:08 +0700

    update

---
+++ app/Services/PdfService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use Mpdf\Mpdf;
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
+
+class PdfService
+{
+    public function generatePdf($html, $filename = 'document.pdf')
+    {
+        $defaultConfig = (new ConfigVariables())->getDefaults();
+        $fontDirs = $defaultConfig['fontDir'];
+        
+        $defaultFontConfig = (new FontVariables())->getDefaults();
+        $fontData = $defaultFontConfig['fontdata'];
+        
+        $mpdf = new Mpdf([
+            'fontDir' => array_merge($fontDirs, [
+                public_path('fonts'),
+            ]),
+            'fontdata' => $fontData + [
+                'examplefont' => [
+                    'R' => 'ExampleFont-Regular.ttf',
+                    'I' => 'ExampleFont-Italic.ttf',
+                ]
+            ],
+            'default_font' => 'dejavusans',
+            'margin_left' => 10,
+            'margin_right' => 10,
+            'margin_top' => 10,
+            'margin_bottom' => 10,
+            'margin_header' => 0,
+            'margin_footer' => 0,
+        ]);
+        
+        $mpdf->WriteHTML($html);
+        
+        return $mpdf->Output($filename, \Mpdf\Output\Destination::DOWNLOAD);
+    }
+}
